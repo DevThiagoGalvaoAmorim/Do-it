d6de4fed0615e2b91a68f2604df07da8df1b07ee
DevThiagoGalvaoAmorim <thiagogalvao524@gmail.com>
2025-04-22 22:39:57 -0300

    Menu lateral interativo

--- www/script.js
+++ www/script.js
@@ -1,21 +1,29 @@
+const toggleBtn = document.querySelector('.toggle-btn');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('expanded');
+  content.classList.toggle('pushed');
+});
+
 function abrirPopup(id) {
-    document.getElementById(id).style.display = 'flex';
+  document.getElementById(id).style.display = 'flex';
 }
-  
+
 function fecharPopup(event, popupEl) {
-    event.stopPropagation();
-    popupEl.style.display = 'none';
+  event.stopPropagation();
+  popupEl.style.display = 'none';
 }
 
 function verificaEnter(event) {
-    if (event.key === 'Enter') {
-      event.preventDefault(); // impede envio se estiver dentro de um form
-      // Chama a função desejada
-      enviarFormulario(); 
-    }
+  if (event.key === 'Enter') {
+    event.preventDefault(); // impede envio se estiver dentro de um form
+    // Chama a função desejada
+    enviarFormulario();
   }
-  
-  function enviarFormulario() {
-    // Lógica de organização aqui
-    alert('Buscando nota...');
-  }
+}
+
+function enviarFormulario() {
+  // Lógica de organização aqui
+  alert('Buscando nota...');
+}
