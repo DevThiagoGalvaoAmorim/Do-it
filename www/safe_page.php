925012e08f94cb4584830a826b254ce99282efd6
João Galvão <joao.galvao@mail.uft.edu.br>
2025-05-12 18:13:57 -0300

    Feature/Controle de Sessao

---
+++ www/safe_page.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+// Verifica se a sessão está ativa e se o usuário está autenticado
+if (!isset($_SESSION['id']) || !isset($_SESSION['nome']) || !isset($_SESSION['email'])) {
+    // Redireciona para a página de login
+    header('Location: login.php');
+    exit;
+}
+?>
