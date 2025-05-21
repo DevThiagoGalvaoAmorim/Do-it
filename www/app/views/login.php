8ba14eecf45b870a75e2f8a878b1eadf861a31ea
Ruam Marcos <ruam.marcos@mail.uft.edu.br>
2025-05-20 21:47:38 -0300

    Colocando Login no MVC
    Co-authored-by: Guilherme737 <guilherme.carvalho1@uft.edu.br>
    Co-authored-by: douglasalvesc <douglas.cruz@uft.edu.br>
    Co-authored-by: DevThiagoGalvaoAmorim <thiagogalvao524@gmail.com>

--- www/app/views/login.php
+++ www/app/views/login.php
@@ -1,35 +1,5 @@
 <?php
-session_start();
-require_once 'conexao_db/conexao.php';
-require_once 'conexao_db/usuarios_crud.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'] ?? '';
-    $senha = $_POST['senha'] ?? '';
-
-    if (!empty($email) && !empty($senha)) {
-        $usuario = buscarUsuario($email, $senha);
-        
-        if ($usuario) {
-            $_SESSION['id'] = $usuario['id'];
-            $_SESSION['nome'] = $usuario['nome'];
-            $_SESSION['email'] = $usuario['email'];
-            $_SESSION['tipo'] = $usuario['tipo'] ?? 'user';
-            
-            // Redirecionar com base no tipo de usuário
-            if (isset($usuario['tipo']) && $usuario['tipo'] === 'admin') {
-                header('Location: admin.php');
-            } else {
-                header('Location: main.php');
-            }
-            exit;
-        } else {
-            echo '<script>alert("Email ou senha incorretos!");</script>';
-        }
-    } else {
-        echo '<script>alert("Preencha todos os campos!");</script>';
-    }
-}
+require_once '../controllers/login_user.php';
 ?>
 
 <!DOCTYPE html>
