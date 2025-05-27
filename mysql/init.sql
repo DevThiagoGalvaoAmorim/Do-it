f95150557c2c3c089c792fda22b84f6d95939312
DevThiagoGalvaoAmorim <thiagogalvao524@gmail.com>
2025-05-27 20:32:59 -0300

    CrudNotificarion

--- mysql/init.sql
+++ mysql/init.sql
@@ -20,3 +20,12 @@ CREATE TABLE notas (
     tipo ENUM('Checklist', 'Anotação') NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS lembrete (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
+    id_usuario INT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios (id) ON DELETE CASCADE
+);
