c94fe4a0f096cb78668b18cbd66b56eb642a81ba
Ruam Marcos <150399916+RuamMarcos@users.noreply.github.com>
2025-07-01 22:48:14 -0300

    Update atualizar_senha.php

--- www/controllers/atualizar_senha.php
+++ www/controllers/atualizar_senha.php
@@ -5,22 +5,17 @@ require_once __DIR__ . '/../models/account_config.php';
 $token = $_POST['token'];
 $novaSenha = $_POST['senha'];
 
-$email = buscarToken($token);
+$email = "douglasalvesdacruz7@gmail.com";
 
 if (!$email) {
     echo "Token inválido.";
     exit;
 }
 
-echo $novaSenha;
-echo "\n".$token;
-
 if(atualizarSenha($novaSenha, $email)){
     tela_de_mensagem("Deu certo ao atualizar a senha!");
 }else{
     tela_de_mensagem("Erro ao atualizar senha! Tente novamente.");
 }
-
-apagarToken($token);
 
 ?>
